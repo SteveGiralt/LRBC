fae14d52dd9c057b2c5362ed638c9f612da2ccc1
Steve Giralt <88471484+SteveGiralt@users.noreply.github.com>
2024-04-02 15:22:57 -0600

    suppress built hints

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,19 +1,5 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-//   theme: {
-//     extend: {
-//       colors: {
-//         "dark-blue": "#224759",
-//         "lrbc-red": "#800020",
-//       },
-//     },
-//   },
-//   plugins: [],
-// };
-
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
     "./node_modules/flowbite/**/*.js",
