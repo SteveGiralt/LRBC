9c4e6650e0cad0713256f1c8ec491d1a78c92311
Steve Giralt <88471484+SteveGiralt@users.noreply.github.com>
2024-08-13 16:09:22 -0600

    json

--- api/index.py
+++ api/index.py
@@ -1,10 +1,12 @@
 from http.server import BaseHTTPRequestHandler
- 
+import json
+
 class handler(BaseHTTPRequestHandler):
- 
+
     def do_GET(self):
+        json_data = {"foo": "bar"}
         self.send_response(200)
-        self.send_header('Content-type','text/plain')
+        self.send_header('Content-type', 'application/json')  # Important: Set content type to JSON
         self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
+        self.wfile.write(json.dumps(json_data).encode('utf-8'))
         return
