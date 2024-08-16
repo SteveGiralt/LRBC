82f5b321d3c64d9057b56f9fae6b7e5ed15a118b
Steve Giralt <88471484+SteveGiralt@users.noreply.github.com>
2024-08-16 15:08:21 -0600

    enable analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-// import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/serverless";
 
 import react from "@astrojs/react";
 
@@ -8,8 +8,8 @@ import react from "@astrojs/react";
 export default defineConfig({
   integrations: [tailwind(), react()],
   site: "https://lonerockbiblechurch.com",
-  // output: "server",
-  // adapter: vercel({
-  //   webAnalytics: { enabled: true },
-  // }),
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 });
