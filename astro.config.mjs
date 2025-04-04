f89dbcff2dfbc3f535cccde170ff84b9727704c9
Steve Giralt <88471484+SteveGiralt@users.noreply.github.com>
2025-04-04 14:34:48 -0600

    Upgrade Astro to next version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel"; // updated import
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
 
 import react from "@astrojs/react";
 
@@ -8,7 +8,7 @@ import react from "@astrojs/react";
 export default defineConfig({
   integrations: [tailwind(), react()],
   site: "https://lonerockbiblechurch.com",
-  output: "hybrid",
+  output: "server",
   adapter: vercel({
     webAnalytics: { enabled: true },
   }),
