b70fac938fa9651dd22d679f86b7f5bb064bba11
Steve Giralt <88471484+SteveGiralt@users.noreply.github.com>
2025-10-03 16:35:56 -0600

    Fix auth_uri assignment: Update auth_uri to use a static URL for Google OAuth2 authentication in calendar handlers.

--- api/youth-calendar/index.py
+++ api/youth-calendar/index.py
@@ -17,7 +17,7 @@ class handler(BaseCalendarHandler):
             "private_key": os.environ.get("CALENDAR_PRIVATE_KEY"),
             "client_email": os.environ.get("CALENDAR_CLIENT_EMAIL"),
             "client_id": os.environ.get("CALENDAR_CLIENT_ID"),
-            "auth_uri": os.environ.get("CALENDAR_CLIENT_EMAIL"),
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
             "token_uri": "https://oauth2.googleapis.com/token",
             "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
             "client_x509_cert_url": os.environ.get("CALENDAR_CLIENT_X509_CERT_URL"),
